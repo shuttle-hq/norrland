a6088289eb3a52ab2ade884b6208c9aea557b2c9
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2024-11-21 05:10:26 +0100

    norrland 0.0.1

---
+++ src/lib.rs
@@ -0,0 +1,140 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{
+    parse_macro_input, parse_quote,
+    punctuated::Punctuated,
+    token::{Comma, Pub, Trait},
+    FnArg, Ident, ImplItem, ItemImpl, ItemTrait, Pat, Path, Signature, TraitItem, TraitItemFn,
+    Type, TypePath, Visibility,
+};
+
+#[proc_macro_attribute]
+pub fn norrland(args: TokenStream, tokens: TokenStream) -> TokenStream {
+    let db_type = parse_macro_input!(args as TypePath);
+    let input = parse_macro_input!(tokens as ItemImpl);
+
+    // Extract the struct name
+    let name = match &input.self_ty.as_ref() {
+        Type::Path(path) => path.path.segments.last().unwrap().ident.clone(),
+        _ => return quote! { compile_error!("Expected an impl block for a named struct"); }.into(),
+    };
+
+    // Extract methods
+    let fns = input
+        .items
+        .iter()
+        .filter_map(|item| match item {
+            ImplItem::Fn(method) => Some(method),
+            _ => None,
+        })
+        .collect::<Vec<_>>();
+
+    // Generate trait with just the function definitions
+    let trait_fns = fns
+        .iter()
+        .map(|func| {
+            TraitItem::Fn(TraitItemFn {
+                attrs: func.attrs.clone(),
+                sig: remove_mut_bindings(func.sig.clone()),
+                default: None,
+                semi_token: Some(Default::default()),
+            })
+        })
+        .collect::<Vec<_>>();
+    let trait_def = ItemTrait {
+        attrs: vec![],
+        vis: Visibility::Public(Pub::default()),
+        unsafety: None,
+        auto_token: None,
+        trait_token: Trait(name.span()),
+        ident: name.clone(),
+        generics: input.generics.clone(),
+        colon_token: None,
+        supertraits: Punctuated::new(),
+        restriction: None,
+        brace_token: Default::default(),
+        items: trait_fns,
+    };
+
+    let pool_impl_fns = fns
+        .clone()
+        .into_iter()
+        .map(|func| {
+            let mut modified_func = func.clone();
+            modified_func.sig = remove_mut_bindings(func.sig.clone());
+
+            let ident = func.sig.ident.clone();
+            let args_to_conn = func
+                .sig
+                .inputs
+                .clone()
+                .into_iter()
+                .filter_map(|a| match a {
+                    FnArg::Typed(typed) => Some(typed),
+                    _ => None,
+                })
+                .filter_map(|pt| match *pt.pat {
+                    Pat::Ident(ident) => Some(ident.ident),
+                    _ => None,
+                })
+                .collect::<Punctuated<Ident, Comma>>();
+            modified_func.block.stmts = parse_quote! {
+                let mut conn = self.acquire().await?;
+                conn.#ident(#args_to_conn).await
+            };
+            modified_func
+        })
+        .collect::<Vec<_>>();
+
+    let (conn, pool): (Path, Path) = match db_type
+        .path
+        .segments
+        .last()
+        .unwrap()
+        .ident
+        .to_string()
+        .as_str()
+    {
+        "Postgres" => (
+            parse_quote!(::sqlx::PgConnection),
+            parse_quote!(::sqlx::PgPool),
+        ),
+        "MySql" => (
+            parse_quote!(::sqlx::MySqlConnection),
+            parse_quote!(::sqlx::MySqlPool),
+        ),
+        _ => return quote! { compile_error!("Expected an sqlx Database type"); }.into(),
+    };
+
+    // Generate trait implementation
+    let impl_trait_block = quote! {
+        type _Dummy = #db_type;
+
+        #trait_def
+
+        impl #name for &mut #conn {
+            #(#fns)*
+        }
+
+        impl #name for &#pool {
+            #(#pool_impl_fns)*
+        }
+    };
+
+    impl_trait_block.into()
+}
+
+fn remove_mut_bindings(mut sig: Signature) -> Signature {
+    sig.inputs.iter_mut().for_each(|i| {
+        if let FnArg::Typed(typed) = i {
+            if let Pat::Ident(ident) = typed.pat.as_mut() {
+                // Remove any (owned) mut binding of arg
+                ident.mutability = None;
+                // Remove subpatterns
+                ident.subpat = None;
+            }
+        }
+    });
+
+    sig
+}
---
+++ tests/main.rs
@@ -0,0 +1,125 @@
+#![allow(async_fn_in_trait)]
+
+use norrland::norrland;
+use sqlx::{query, Acquire, Postgres};
+
+#[norrland(Postgres)]
+impl MyDB {
+    async fn select(self, a: i32) -> Result<(), sqlx::Error> {
+        query("SELECT num FROM numbers WHERE num = $1")
+            .bind(a)
+            .fetch_one(self.as_mut())
+            .await?;
+
+        Ok(())
+    }
+    async fn insert_with_internal_trx(self, a: i32) -> Result<(), sqlx::Error> {
+        let mut trx = self.begin().await?;
+        query("INSERT INTO numbers VALUES ($1)")
+            .bind(a)
+            .execute(trx.as_mut())
+            .await?;
+        trx.commit().await?;
+
+        Ok(())
+    }
+    async fn insert_with_internal_trx_fails(self, a: i32) -> Result<(), sqlx::Error> {
+        let mut trx = self.begin().await?;
+        query("INSERT INTO numbers VALUES ($1)")
+            .bind(a)
+            .execute(trx.as_mut())
+            .await?;
+        query("SELECT ???").execute(trx.as_mut()).await?; // fails
+        trx.commit().await?;
+
+        Ok(())
+    }
+    async fn noop(self) -> Result<(), sqlx::Error> {
+        Ok(())
+    }
+    async fn multi_args(self, a: i32, b: &mut i32, mut c: String) -> Result<(), sqlx::Error> {
+        c.push('a');
+        let _ = a + *b + c.len() as i32;
+        Ok(())
+    }
+    async fn destructure(
+        self,
+        _a1 @ A { b, c }: &A,
+        // A { b: d, c: e }: A, // TODO
+    ) -> Result<(), sqlx::Error> {
+        let _ = *b + *c as i32;
+        // let _ = d + e as i32;
+        Ok(())
+    }
+    // TODO
+    // async fn wild(self, _: bool) -> Result<(), sqlx::Error> {
+    //     Ok(())
+    // }
+}
+pub struct A {
+    pub b: i32,
+    pub c: bool,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::MyDB;
+    use sqlx::{query, PgPool};
+    use testcontainers_modules::{
+        postgres::Postgres,
+        testcontainers::{runners::AsyncRunner, ContainerAsync, ImageExt},
+    };
+    use tracing::Level;
+
+    async fn setup() -> (ContainerAsync<Postgres>, PgPool) {
+        let container = Postgres::default()
+            .with_tag("16-alpine")
+            .start()
+            .await
+            .unwrap();
+
+        let conn_str = format!(
+            "postgres://postgres:postgres@{}:{}/postgres",
+            container.get_host().await.unwrap(),
+            container.get_host_port_ipv4(5432).await.unwrap(),
+        );
+        let pool = PgPool::connect(&conn_str).await.unwrap();
+
+        query("CREATE TABLE numbers (num INT)")
+            .execute(&pool)
+            .await
+            .unwrap();
+
+        (container, pool)
+    }
+
+    #[tokio::test]
+    async fn external_plus_internal_trx() {
+        tracing_subscriber::fmt()
+            .with_max_level(Level::DEBUG)
+            .init();
+        let (_cont, pool) = setup().await;
+
+        // combine several db method calls in one transaction
+        // transactions internal to db methods run inside this transaction
+        let mut trx = pool.begin().await.unwrap();
+        trx.as_mut().insert_with_internal_trx(1).await.unwrap();
+        trx.as_mut().select(1).await.unwrap();
+        trx.as_mut()
+            .insert_with_internal_trx_fails(99)
+            .await
+            .unwrap_err();
+        trx.as_mut().select(99).await.unwrap_err();
+        trx.commit().await.unwrap();
+        pool.select(1).await.unwrap();
+
+        // outer transaction rolls back
+        let mut trx = pool.begin().await.unwrap();
+        trx.as_mut().select(2).await.unwrap_err();
+        trx.as_mut().insert_with_internal_trx(2).await.unwrap();
+        trx.as_mut().select(2).await.unwrap();
+        trx.as_mut().select(3).await.unwrap_err();
+        trx.rollback().await.unwrap();
+        pool.select(2).await.unwrap_err();
+    }
+}
